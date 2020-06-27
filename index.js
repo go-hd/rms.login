edc86c54a6478bf43138c860c61fdba9709f4efe
河本 勇人 <y.kawamoto@go-hd.jp>
2020-06-28 04:38:41 +0900

    Add getShopId function.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-module.exports = { login }
+module.exports = { login, getShopId }
 
 const selectors = {
   loginIdInput: 'input[name=login_id]',
@@ -8,10 +8,12 @@ const selectors = {
   userPasswordInput: 'input[name=user_passwd]',
   userHistoryCheckbox: 'input[name=HSTRYRUSR]',
   submitButton: 'button[type=submit],button[name=submit]',
+  shopPageLink: '#com_header_shop_url',
 }
 
 const urls = {
   login: 'https://glogin.rms.rakuten.co.jp/?sp_id=1',
+  mainmenu: 'https://mainmenu.rms.rakuten.co.jp',
 }
 
 /**
@@ -47,9 +49,23 @@ async function login (page, shopId, shopPassword, userId, userPassword) {
  * @param page {import("puppeteer/lib/Page").Page} Puppeteer page instance.
  * @returns {Promise<void>}
  */
-function submit(page) {
+function submit (page) {
   return Promise.all([
     page.waitForNavigation({ waitUntil: 'domcontentloaded' }),
     page.click(selectors.submitButton)
   ])
+}
+
+/**
+ * Get the shop ID.
+ *
+ * @param page {import("puppeteer/lib/Page").Page} Puppeteer page instance.
+ * @returns {Promise<string>}
+ */
+async function getShopId (page) {
+  await page.goto(urls.mainmenu)
+  return page.evaluate(selector => {
+    const shopPageURL = document.querySelector(selector).getAttribute('href')
+    return shopPageURL.replace(/^https:\/\/www.rakuten.co.jp\/(.+)$/, '$1')
+  }, selectors.shopPageLink)
 }
--- test.js
+++ test.js
@@ -1,12 +1,15 @@
 const puppeteer = require('puppeteer')
+const { describe, before, after, it } = require('mocha')
 const { expect } = require('chai')
 require('dotenv').config()
 
-const { login } = require('./index.js')
+const { login, getShopId } = require('./index.js')
 
 describe('Login', function () {
-  it('Response is 200', async function () {
-    const browser = await puppeteer.launch({
+  this.timeout(0)
+
+  before(async function () {
+    global.browser = await puppeteer.launch({
       args: [
         '--disable-gpu',
         '--disable-dev-shm-usage',
@@ -14,30 +17,42 @@ describe('Login', function () {
         '--no-first-run',
         '--no-sandbox',
         '--no-zygote',
-        '--single-process'
-      ]
+        '--single-process',
+      ],
     })
 
-    const page = await browser.newPage()
-    await page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36')
-    await page.setRequestInterception(true)
-    page.on('request', (request) => {
+    global.page = await global.browser.newPage()
+    await global.page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36')
+    await global.page.setRequestInterception(true)
+
+    global.page.on('request', (request) => {
       const isAbortType = type => ['image', 'stylesheet', 'font'].includes(type)
       isAbortType(request.resourceType()) ? request.abort() : request.continue()
     })
 
     await login(
-      page,
+      global.page,
       process.env.RMS_SHOP_ID,
       process.env.RMS_SHOP_PASSWORD,
       process.env.RMS_USER_ID,
       process.env.RMS_USER_PASSWORD,
     )
+  })
 
-    const response = await page.goto('https://mainmenu.rms.rakuten.co.jp', { waitUntil: 'load' })
+  after(async function () {
+    await global.browser.close()
+  })
+
+  it('Status', async function () {
+    const response = await global.page.goto('https://mainmenu.rms.rakuten.co.jp', { waitUntil: 'load' })
     const status = await response.status()
-    expect(status).to.equal(200)
 
-    await browser.close()
+    expect(status === 200)
+  })
+
+  it('Correct shop', async function () {
+    const shopId = await getShopId(global.page)
+
+    expect(shopId === process.env.RAKUTEN_SHOP_URL)
   })
 })
